c3e35f0a019ce08c943cb76a962e300f384fdd18
Piotr Bielec <bielec.piotr@gmail.com>
2020-08-10 18:32:35 +0200

    add 'active' class to clicked link

--- js/script.js
+++ js/script.js
@@ -8,10 +8,11 @@ document.getElementById('test-button').addEventListener('click', function(){
 */
 
 const titleClickHandler = function(event){
+    const clickedElement = this;
     console.log('Link was clicked!');
-    console.log('event: ' + event);
+    console.log('event: ', event);
 
-    /* remove class 'active' from all article links  */
+    /* [DONE] remove class 'active' from all article links  */
 
     const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -19,9 +20,14 @@ const titleClickHandler = function(event){
         activeLink.classList.remove('active');
     }
 
-    /* add class 'active' to the clicked link */
+    /* [IN PROGRESS] add class 'active' to the clicked link */
 
-    /* remove class 'active' from all articles */
+    console.log('clickedElement: ', clickedElement);
+    console.log('clickedElement with plus: ' + clickedElement);
+
+    clickedElement.classList.add('active');
+
+    /* [DONE] remove class 'active' from all articles */
 
     const activeArticles = document.querySelectorAll('.posts article.active');
 
