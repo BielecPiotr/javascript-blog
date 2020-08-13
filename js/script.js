35b3305bfc9016f87a86e7ff1591a6b5e88e0c31
Piotr Bielec <bielec.piotr@gmail.com>
2020-08-13 21:28:41 +0200

    add generateTags skeleton

--- js/script.js
+++ js/script.js
@@ -49,6 +49,7 @@
 
   const optArticleSelector = '.post',
     optTitleSelector = '.post-title',
+    optArticleTagsSelector = '.post-tags .list',
     optTitleListSelector = '.titles';
 
   const generateTitleLinks = function(){
@@ -98,5 +99,34 @@
   };
 
   generateTitleLinks();
+
+  const generateTags = function(){
+    /* find all articles */
+
+    /* START LOOP: for every article: */
+
+    /* find tags wrapper */
+
+    /* make html variable with empty string */
+
+    /* get tags from data-tags attribute */
+
+    /* split tags into array */
+
+    /* START LOOP: for each tag */
+
+    /* generate HTML of the link */
+
+    /* add generated code to html variable */
+
+    /* END LOOP: for each tag */
+
+    /* insert HTML of all the links into the tags wrapper */
+
+    /* END LOOP: for every article: */
+  };
+
+  generateTags();
+
 
 }
